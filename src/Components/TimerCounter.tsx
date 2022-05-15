163c80c20e4c75d2308a557f392a66053053921d
Rhermez10 <ramta97@hotmail.com>
2022-05-15 12:37:11 +0200

    #4 created countdown timer

--- src/Components/TimerCounter.tsx
+++ src/Components/TimerCounter.tsx
@@ -1,18 +1,36 @@
-import  { useState } from "react";
+import  { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 function TimerCounter(){
     const location = useLocation();
     const time = location.state as any
 
-    const [fulltime,setFulltime] =useState<Number |any>();
-
-
+ 
+    const [minute,setMinute] =useState<Number |any>(time.minute);
+    const [second,setSecond] =useState<Number |any>(time.second);
+    useEffect(()=>{
+        let timer=  setInterval(()=>{
+           if (second > 0){
+            setSecond(second-1)
+         }
+         if (second ===0){
+             if(minute ===0){
+                 clearInterval(timer)
+             }else{
+                 setMinute(minute -1)
+                 setSecond(59);
+             }
+         }
+        },1000)
+        return()=>{
+            clearInterval(timer)
+        }
+    });
+ 
     return(
         <section>
-            {time.minute}:
-            {time.second}
-
+            {minute}:
+            {second}
         </section>
     )
 }
