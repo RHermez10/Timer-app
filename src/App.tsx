b6eb36276018583115e471c6ccc7347efecd7859
Rhermez10 <ramta97@hotmail.com>
2022-05-18 23:09:40 +0200

    some small changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import AnalogTimer from './Components/AnalogTimer';
 import FirstPage from './Components/FirstPage';
+import SetTimer from './Components/SetTimer';
 import TimerCounter from './Components/TimerCounter';
-import TimeSet from './Components/TimeSet';
+
 import TimesUp from './Components/TimesUp';
 
 
@@ -15,7 +16,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<FirstPage header="Intervall" text="For all your timing needs"/>}></Route>
-          <Route path="/TimeSet" element={<TimeSet/>}></Route>
+          <Route path="/TimeSet" element={<SetTimer min={0} sec={0} />}></Route>
           <Route path="/TimerCounter" element={<TimerCounter/>}></Route>
           <Route path="/TimesUp" element={<TimesUp text="Times up!"/>}></Route>
           <Route path="/AnalogTimer" element={<AnalogTimer btn="ABORT TIMER"/>}></Route>
